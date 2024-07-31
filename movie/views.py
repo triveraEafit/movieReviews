fba224f813953115bea0687b595d4473d1903182
triveraEafit <tgananr@eafit.edu.co>
2024-07-31 13:23:00 -0500

    peliculas agregadas

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {'name': 'Tomas G rivera'})
+    #return HttpResponse('<h1>Welcome to home Page</h1>')
+    #return render(request, 'home.html')
+    #return render(request, 'home.html', {'name': 'daramirez9'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
+
+
 def about(request):
-    return render(request, 'about.html',)
+    return render(request, 'about.html')
+
+
+
+
+
